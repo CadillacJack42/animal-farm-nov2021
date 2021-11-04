f60d49e9b3574322298c969f4aa3951955334a9d
James Keicher <jamesdkeicher@gmail.com>
2021-11-04 15:00:57 -0700

    added keydown options

--- app.js
+++ app.js
@@ -12,23 +12,33 @@ const lastPlay = document.getElementById('last-sound')
 // initialize global state
 
 catPic.addEventListener('click', () => {
-  meow.play();
-  lastPlay.textContent = 'You played the sound of a cat';
-})
+    meow.play();
+    lastPlay.textContent = 'You played the sound of a cat';
+});
 
 
 dogPic.addEventListener('click', () => {
-  woof.play();
-  lastPlay.textContent = 'You played the sound of a dog (Good Choice)';
-})
+    woof.play();
+    lastPlay.textContent = 'You played the sound of a dog (Good Choice)';
+});
 
 
 horsePic.addEventListener('click', () => {
-  neigh.play();
-  lastPlay.textContent = 'You played the sound of a horse';
-})
+    neigh.play();
+    lastPlay.textContent = 'You played the sound of a horse';
+});
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'c'){
+        meow.play();
+        lastPlay.textContent = 'You played the sound of a cat';
+    } else if (e.key === 'd'){
+        woof.play();
+        lastPlay.textContent = 'You played the sound of a dog (Good Choice)';
+
+    } else if (e.key === 'h'){
+        neigh.play();
+        lastPlay.textContent = 'You played the sound of a horse';
+
+    }
+});
